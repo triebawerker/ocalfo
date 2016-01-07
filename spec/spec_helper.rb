27c90681d9734ceda3ed2e4f76ffb7aaf1c624c1
micha trieba <micha.trieba@mayflower.de>
2016-01-07 23:55:46 +0100

    and removed bundler from helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,9 @@ require 'ingredient'
 require 'good'
 require 'rspec-html-matchers'
 
+require 'bundler'
+Bundler.require
+
 RSpec.configure do |config|
   config.include Rack::Test::Methods
   config.run_all_when_everything_filtered = true
